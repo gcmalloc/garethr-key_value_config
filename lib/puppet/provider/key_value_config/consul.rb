4389a56637cf199d97ff51b951fe8bd1e62ef5d2
gcmalloc <gcmalloc@gmail.com>
2015-11-28 21:26:29 +0100

    removing dependencies

--- lib/puppet/provider/key_value_config/consul.rb
+++ lib/puppet/provider/key_value_config/consul.rb
@@ -1,27 +1,33 @@
-require 'diplomat'
-
 Puppet::Type.type(:key_value_config).provide(:consul) do
   confine feature: :consul
 
   mk_resource_methods
 
+  CONSUL_HOST = 'localhost'
+  CONSUL_PORT = 8086
+
   def exists?
     Puppet.info("Checking if key #{name} exists")
     begin
-      resp = Diplomat.get(name)
-      resp == resource[:value]
-    rescue Faraday::ResourceNotFound
+      http = Net::HTTP.new(CONSUL_HOST, CONSUL_PORT)
+      http.get('/v2/key/' + name)
+      parsed_resp = JSON::parse(resp.body)
+      parsed_resp[:value] == resource[:value]
+      return 
+    rescue 
       false
     end
   end
 
   def create
     Puppet.info("Setting #{name} to #{resource[:value]}")
-    Diplomat.put(name, resource[:value])
+      http = Net::HTTP.new(CONSUL_HOST, CONSUL_PORT)
+      http.post('key' + name)
   end
 
   def destroy
     Puppet.info("Deleting #{name}")
-    Diplomat.delete(name)
+      http = Net::HTTP.new(CONSUL_HOST, CONSUL_PORT)
+      http.delete('key' + name)
   end
 end
